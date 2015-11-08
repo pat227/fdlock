997d0c87d2ad62f686453277e96b1accf7f95ca0
paul <paul@guerriere>
2015-11-08 11:09:55 -0500

    Forgot to truncate lock file for unit test. 	modified:   unit_test_flock.ml

--- unit_test_flock.ml
+++ unit_test_flock.ml
@@ -1,23 +1,21 @@
 open OUnit
 open Core.Std
 open Flock
-open Flock_t
-open Flock_j
-module F = Flock
 module TestLock = struct
   let lockfilename = "mylock";;
+  exception Testerror of string
   let dne_already fname =
     let open Core.Std.Unix in
     let r = access fname [`Read;`Write;`Exists] in
     match r with
     | Ok () -> false
     | Error _ -> true;;
-  let alock = F.create lockfilename;;
+  let alock = Flock.create lockfilename;;
   let leaselength = None;;
   let testAcquire_test1 fname =
-    F.acquire (F.create fname) None;;
+    Flock.acquire (Flock.create fname) None;;
   let testAcquire fname lexp =
-    F.acquire (F.create fname) lexp;;
+    Flock.acquire (Flock.create fname) lexp;;
   let cleanUp fname =
     try
       Core.Std.Unix.remove fname
@@ -32,16 +30,19 @@ module TestLock = struct
       match thesize with
       | Some size -> let s = (String.create size) in
 		     let _ = read fd ~buf:s ~len:size in
-		     let l = t_of_string s in
-		     let l2 = { l with pid=newpid } in
-		     let serialized = string_of_t l2 in
-		     let _ = lseek fd (Int64.of_int 0) SEEK_SET in
-		     let _ = single_write fd serialized in
-		     (*let _ = ftruncate fd (Int64.of_int (Core.Std.String.length serialized)) in
-		     let s = Core.Std.Time.Span.of_int_sec 1 in
-		     let _  = Core.Std.Time.pause s in*)
-		     let _ = printf "\nPID updated, wrote %s" serialized in
-		     true
+		     let lopt = Flock.of_yojson (Yojson.Safe.from_string s) in
+		     (match lopt with
+		      | `Ok l -> 
+			 let l2 = { l with pid=newpid } in
+			 let serialized = Yojson.Safe.to_string (Flock.to_yojson l2) in
+			 let _ = lseek fd (Int64.of_int 0) SEEK_SET in
+			 let _ = single_write fd serialized in
+			 let _ = ftruncate fd (Int64.of_int (Core.Std.String.length serialized)) in
+  		         (*let s = Core.Std.Time.Span.of_int_sec 1 in
+		         let _  = Core.Std.Time.pause s in*)
+			 let _ = printf "\nPID updated, wrote %s" serialized in
+			 true
+		      | `Error s -> raise (Testerror "index 45, failed to marhsall json"))
       | None -> false in
     let r = access lockpath [`Read;`Write;`Exists] in
     match r with
